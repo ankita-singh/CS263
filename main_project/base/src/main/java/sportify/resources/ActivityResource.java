7bf50fb6bae4cba50b5f88fd55b0e1f942f31eb9
Ankita Singh <ankitasingh.2309@gmail.com>
2016-03-11 14:27:10 -0800

    integrating with memcache

--- main_project/base/src/main/java/sportify/resources/ActivityResource.java
+++ main_project/base/src/main/java/sportify/resources/ActivityResource.java
@@ -3,6 +3,7 @@ package sportify.resources;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List; 
+import java.util.logging.Level;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -36,16 +37,22 @@ import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 
+import com.google.appengine.api.memcache.ErrorHandlers;
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+
 import com.google.appengine.api.users.*;
 
 @Path("/activity")
 public class ActivityResource {
 	
-DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 	UserService userService = UserServiceFactory.getUserService();
+	MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
 
 
-	@GET
+/*	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<Activity> getActivity()
 		{
@@ -65,7 +72,7 @@ DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 			
 		}
 		return activities;	
-	}
+	}*/
 
 
 	@GET
@@ -96,6 +103,36 @@ DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 			return activities;	
 	}
 	
+
+
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Activity> getAllactivities(){
+		Query q = new Query("ACTIVITY");
+		PreparedQuery pq = datastore.prepare(q);
+		ArrayList<Activity> activities;
+		String CACHE_KEY = "ALL_ACTIVITIES";
+		long before = new Date().getTime();
+		activities =  (ArrayList<Activity>) syncCache.get(CACHE_KEY);
+		if(activities == null){
+			activities = new ArrayList<Activity>();
+			for (Entity activityEntity : pq.asIterable()) {
+				Activity activity = new Activity();
+				activity.setName((String)activityEntity.getProperty("name"));
+				activity.setDescription((String)activityEntity.getProperty("description"));
+				activity.setOwnerId((String)activityEntity.getProperty("ownerId"));
+				activities.add(activity);
+			}
+			syncCache.put(CACHE_KEY, activities);
+		}
+		
+		long after = new Date().getTime();
+		System.out.println("Time taken to get all activities "+ (after - before));
+		return activities;
+	}
+
+
 
 
 
